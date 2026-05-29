fc6bfaf4b90b91e7a5a93a811e547413d073ac74
Arjabi04 <arjabishrestha@gmail.com>
2026-05-29 15:35:36 +0545

    fix: update CORS origin to include production URL

--- backend/server.js
+++ backend/server.js
@@ -83,7 +83,7 @@ const sendMarketplacePurchaseEmails = async ({ buyerEmail, sellerEmail, productT
 };
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:5173'], 
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:5173','https://skill-nest-new.vercel.app'], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-token'],
